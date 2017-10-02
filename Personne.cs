7a8c685767db234fbcb4abd55bcb66a956bde301
Anthony VIGIER <anthony.vigier.24@gmail.com>
2017-10-02 14:25:44 +0200

    Ajout classe personne

---
+++ Personne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainPersonne
+{
+    class Personne
+    {
+        public string Nom { set; get; }
+        public string Prenom { set; get; }
+
+        public Personne(string nom, string prenom)
+        {
+            this.Nom = nom;
+            this.Prenom = prenom;
+        }
+
+    }
+}
